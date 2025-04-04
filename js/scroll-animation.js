94cd43f6ef795f30077c5fcf7d29382de3683933
luca2506 <luca.dosche@gmail.com>
2025-04-04 18:26:03 +0200

    2

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,1 +1,17 @@
- 
+ // Alle Icons selektieren
+const icons = document.querySelectorAll('.circle-icon');
+
+// Scroll-Event
+window.addEventListener('scroll', () => {
+  icons.forEach(icon => {
+    const rect = icon.getBoundingClientRect();
+    const triggerPoint = window.innerHeight * 0.8;
+
+    if (rect.top < triggerPoint) {
+      icon.classList.add('active');
+    } else {
+      icon.classList.remove('active');
+    }
+  });
+});
+
